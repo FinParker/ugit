4d17640c5f8f514e5ad1e59b2cf2c9a6b696a52e
iming <2576226012@qq.com>
2025-07-24 14:29:10 +0800

    add ugit init

---
+++ README.md
@@ -0,0 +1,19 @@
+# ugit
+
+## develop
+
+```bash
+pip install -e .
+```
+
+## usage
+
+```bash
+ugit --debug init
+```
+
+## example
+
+```bash
+ugit --debug init
+```
--- ugit/cli.py
+++ ugit/cli.py
@@ -1,18 +1,32 @@
 import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 def main ():
     args = parse_args()
+    logging.debug('args: %s',args)
     args.func(args)
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('--debug', action='store_true', help='Enable debug mode')
+
     commands = parser.add_subparsers(dest='command')
     commands.required = True
+    # args.command = 'init'
 
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
+    # args.func = init
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.getLogger().setLevel(logging.DEBUG)
+
+    return args
 
 def init(args):
-    print('init process begin')
+    logging.debug('init process begin')
